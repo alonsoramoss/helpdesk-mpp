39a3347e72c35141d54b76addfb5d03a94e94ca4
Alonso Ramos <alonso24005@hotmail.com>
2025-01-29 13:23:32 -0500

    refactor: actualiza la funcionalidad de cambio de tema y mejora el manejo de la transición

--- src/components/cambio-tema.tsx
+++ src/components/cambio-tema.tsx
@@ -2,22 +2,24 @@
 
 import * as React from "react";
 import { useTheme } from "next-themes";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-
+import { Sun, Moon } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from "@/components/ui/tooltip";
 
 export function CambioTema() {
   const { setTheme, theme } = useTheme();
 
-  const desactivTransition = () => {
-    document.body.classList.add("no-transition");
+  const handleThemeChange = () => {
+    const root = document.documentElement;
+    root.classList.add("desactivar-transicion");
+
+    setTheme(theme === "dark" ? "light" : "dark");
+
     setTimeout(() => {
-      document.body.classList.remove("no-transition");
-    }, 1);
+      root.classList.remove("desactivar-transicion");
+    }, 100);
   };
-  desactivTransition();
-  
+
   return (
     <TooltipProvider disableHoverableContent>
       <Tooltip delayDuration={100}>
@@ -26,10 +28,10 @@ export function CambioTema() {
             className="rounded-full w-8 h-8 bg-background mr-2"
             variant="outline"
             size="icon"
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            onClick={handleThemeChange}
           >
-            <SunIcon className="w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
-            <MoonIcon className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
+            <Sun className="w-[1.2rem] h-[1.2rem] dark:hidden" />
+            <Moon className="absolute w-[1.2rem] h-[1.2rem] dark:block hidden" />
             <span className="sr-only">Cambiar Tema</span>
           </Button>
         </TooltipTrigger>
