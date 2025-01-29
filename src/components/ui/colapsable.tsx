866c6eb0901eda30c09be94d8f6b41d32ef50a7c
Alonso Ramos <alonso24005@hotmail.com>
2025-01-29 09:27:51 -0500

    feat: agrega componente submenu-button y colapsable

---
+++ src/components/ui/colapsable.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+const Colapsable = CollapsiblePrimitive.Root
+
+const ColapsableTrigger = CollapsiblePrimitive.CollapsibleTrigger
+
+const ColapsableContent = CollapsiblePrimitive.CollapsibleContent
+
+export { Colapsable, ColapsableTrigger, ColapsableContent }
