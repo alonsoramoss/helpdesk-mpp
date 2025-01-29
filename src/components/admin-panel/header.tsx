dc3fb92023e1b25eee416b45f6e4dce6a84d6c18
Alonso Ramos <alonso24005@hotmail.com>
2025-01-29 09:32:42 -0500

    feat: reemplaza SheetMenu con MenuMovil y agrega fecha actual

--- src/components/admin-panel/header.tsx
+++ src/components/admin-panel/header.tsx
@@ -1,24 +1,37 @@
 import { CambioTema } from "@/components/cambio-tema";
 import { UserHeader } from "@/components/admin-panel/user-header";
-import { SheetMenu } from "@/components/admin-panel/sheet-menu";
+import { MenuMovil } from "@/components/admin-panel/menu-movil";
 
 interface HeaderProps {
   title: string;
 }
 
 export function Header({ title }: HeaderProps) {
+  const fechaActual = new Date().toLocaleDateString("es-ES", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
   return (
-    <header className="sticky top-0 z-10 w-full shadow bg-primary dark:shadow-secondary">
-      <div className="mx-4 sm:mx-8 flex h-14 items-center">
-        <div className="flex items-center space-x-4 lg:space-x-0">
-          <SheetMenu />
-          <h1 className="font-bold">{title}</h1>
+    <>
+      <header className="sticky top-0 z-10 w-full bg-gradient-to-r from-primary to-indigo-700">
+        <div className="mx-4 sm:mx-8 flex h-14 items-center">
+          <div className="flex items-center space-x-4 lg:space-x-0">
+            <MenuMovil />
+            <h1 className="font-bold">{title}</h1>
+          </div>
+          <div className="flex flex-1 items-center justify-end">
+            <CambioTema />
+            <UserHeader />
+          </div>
         </div>
-        <div className="flex flex-1 items-center justify-end">
-          <CambioTema />
-          <UserHeader />
-        </div>
+      </header>
+      
+      <div className="bg-neutral-200 dark:bg-neutral-800 py-2">
+        <p className="flex justify-center items-center font-medium">{fechaActual}</p>
       </div>
-    </header>
+    </>
   );
 }
