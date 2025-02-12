a6d4a20cc980f612c10f3dcc6b03f16bd117a076
Alonso Ramos <alonso24005@hotmail.com>
2025-02-12 09:18:07 -0500

    refactor: mejora el diseño y el contenido del componente NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,11 +9,9 @@ export default function NotFound() {
     <>
       <div className="absolute inset-0 flex items-center justify-center mb-20">
         <div className="text-center p-8 md:p-16 w-full max-w-2xl">
-          <h1 className="text-5xl md:text-6xl font-black mb-5"> #404 </h1>
-          <p className="text-lg md:text-2xl font-semibold"> Página no encontrada </p>
-          <a href="/inicio" className="mt-5 inline-block px-5 py-2 bg-blue-600 text-white rounded hover:bg-blue-800 transition duration-300">
-            Volver al Inicio
-          </a>
+          <h1 className="text-8xl md:text-9xl font-black"> 404 </h1>
+          <p className="text-lg md:text-xl font-bold my-1"> Lo sentimos. No podemos encontrar esa página. </p>
+          <p className="text-sm md:text-base">Verifique la dirección o inténtelo nuevamente.</p>
         </div>
       </div>
     </>
