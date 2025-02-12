646ed89a13f02066e98b93631927a106436169f4
Alonso Ramos <alonso24005@hotmail.com>
2025-02-12 09:27:12 -0500

    refactor: reorganiza la página principal y utiliza el componente Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,16 @@
+"use client"
 
-function Login() {
+import Login from "@/components/auth/login";
+
+export default function Page() {
   return (
-
-    <div className=" flex flex-col md:flex-row h-screen">
-      <div className="w-full md:w-2/3 md:block hidden">
-        <div className="h-full flex justify-center items-center">
-          <div
-            className="bg-cover bg-center h-full w-full"
-            style={{
-              backgroundImage: `url("placeholder.png")`,
-            }}
-          />
-        </div>
+    <main className="flex items-center justify-center min-h-screen"
+      style={{
+        background: "radial-gradient(circle at center, #00ffff, #148eb3)",
+      }}>
+      <div className="relative bg-white m-5 p-5 sm:p-10 rounded-xl shadow-xl w-full max-w-lg">
+        <Login />
       </div>
-
-      <div className="w-full md:w-1/3 flex justify-center items-center h-screen">
-        <div className="max-w-md w-full">
-          <a href="/inicio">
-            <h2 className="text-3xl font-bold text-center uppercase">Iniciar Sesión</h2>
-          </a>
-          <div className="p-4 rounded-md overflow-hidden shadow-md">
-          </div>
-        </div>
-      </div>
-    </div >
+    </main>
   )
 }
-
-export default Login
