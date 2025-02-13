b389327864d907a17efc9a0d97c7436540af966b
Alonso Ramos <alonso24005@hotmail.com>
2025-02-13 08:32:13 -0500

    feat: agrega imágenes y mejora el diseño de page base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import Login from "@/components/auth/login";
 
 export default function Page() {
   return (
-    <main className="flex items-center justify-center min-h-screen"
+    <main className="flex items-center justify-center min-h-screen bg-cover bg-no-repeat"
       style={{
-        background: "radial-gradient(circle at center, #00ffff, #148eb3)",
+        backgroundImage: "url('/fondomuni.jpg')",
+        // background: "radial-gradient(circle at center, #00ffff, #148eb3)",
       }}>
-      <div className="relative bg-white m-5 p-5 sm:p-10 rounded-xl shadow-xl w-full max-w-lg">
+      <div className="m-5 rounded-xl shadow-2xl w-full max-w-lg">
         <Login />
       </div>
     </main>
