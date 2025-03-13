bc5d35aa70b16d74e6442a654c4a83105d66a507
Alonso Ramos <alonso24005@hotmail.com>
2025-03-13 10:29:37 -0500

    feat: actualiza el componente reporte-general-content para mostrar gráficos de incidentes

--- src/components/content/reporte-general-content/reporte-general-content.tsx
+++ src/components/content/reporte-general-content/reporte-general-content.tsx
@@ -1,20 +1,26 @@
+"use client"
+
 import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
+import IncidentesAtencionBarra from "./charts/incidentes-atencion-barra";
+import IncidentesEstadoPie from "./charts/incidentes-estado-pie";
+import IncidentesLinea from "./charts/incidentes-linea";
+import IncidentesEstadoArea from "./charts/incidentes-estado-area";
 
 export default function ReporteGeneralContent() {
   return (
     <Card>
       <CardContent>
-        <div className="flex">
-          <div>
-            REPORTE GENERAL
-            <Image
-              src="/MuniPisco.png"
-              alt="Municipalidad de Pisco"
-              width={500}
-              height={500}
-              priority
-            />
+        <div className="my-5 text-center text-2xl font-bold">
+          REPORTE DE INCIDENTES
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="grid gap-4 md:grid-cols-2">
+            <IncidentesAtencionBarra  />
+            <IncidentesEstadoPie  />
+          </div>
+          <div className="grid gap-4 md:grid-cols-2">
+            <IncidentesLinea  />
+            <IncidentesEstadoArea  />
           </div>
         </div>
       </CardContent>
