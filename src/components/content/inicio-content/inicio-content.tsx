417cb60992fe4ca59ac21f4d4d62d3e0cbe4760b
Alonso Ramos <alonso24005@hotmail.com>
2025-03-13 10:32:55 -0500

    feat: reorganiza el componente inicio-content y muestra alertas y tabla de incidentes

--- src/components/content/inicio-content/inicio-content.tsx
+++ src/components/content/inicio-content/inicio-content.tsx
@@ -1,30 +1,16 @@
 "use client"
 
 import { Card, CardContent } from "@/components/ui/card";
-import IncidentesAlertas from "./charts/incidentes-alertas";
-import IncidentesTable from "../../data/incidentesTable";
-import IncidentesLinea from "./charts/incidentes-linea";
-import IncidentesAtencionBarra from "./charts/incidentes-atencion-barra";
-import IncidentesEstadoPie from "./charts/incidentes-estado-pie";
-import IncidentesEstadoArea from "./charts/incidentes-estado-area";
+import IncidentesAlertas from "./incidentes-alertas";
+import IncidentesTable from "@/components/data/incidentesTable";
 
 export default function InicioContent() {
   return (
     <Card>
       <CardContent>
         <IncidentesAlertas />
-        <div className="flex flex-col gap-4">
-          <div className="w-full">
-            <IncidentesTable />
-          </div>
-          <div className="grid gap-4 md:grid-cols-2">
-            <IncidentesAtencionBarra  />
-            <IncidentesEstadoPie  />
-          </div>
-          <div className="grid gap-4 md:grid-cols-2">
-            <IncidentesLinea  />
-            <IncidentesEstadoArea  />
-          </div>
+        <div className="flex justify-center">
+          <IncidentesTable />
         </div>
       </CardContent>
     </Card>
