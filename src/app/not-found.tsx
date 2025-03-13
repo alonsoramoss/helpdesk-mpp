cc67bc5247a644c50d410fcac9aadb24ecfd6a27
Alonso Ramos <alonso24005@hotmail.com>
2025-03-13 08:43:37 -0500

    feat: agrega color primario al encabezado de la página 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
     <>
       <div className="absolute inset-0 flex items-center justify-center mb-20">
         <div className="text-center p-8 md:p-16 w-full max-w-2xl">
-          <h1 className="text-8xl md:text-9xl font-black"> 404 </h1>
+          <h1 className="text-8xl md:text-9xl font-black text-primary"> 404 </h1>
           <p className="text-lg md:text-xl font-bold my-1"> Lo sentimos. No podemos encontrar esa página. </p>
           <p className="text-sm md:text-base">Verifique la dirección o inténtelo nuevamente.</p>
         </div>
