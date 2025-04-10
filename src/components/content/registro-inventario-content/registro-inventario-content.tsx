96d57585ab76b36049729458eb74d7ff459744e8
Alonso Ramos <alonso24005@hotmail.com>
2025-04-10 11:36:36 -0500

    refactor: comenta imports y components que aún no se utilizan

--- src/components/content/registro-inventario-content/registro-inventario-content.tsx
+++ src/components/content/registro-inventario-content/registro-inventario-content.tsx
@@ -1,11 +1,11 @@
-"use client"
+// "use client"
 
-import FormularioRegistroInventario from "./formulario-registro-inventario";
+// import FormularioRegistroInventario from "./formulario-registro-inventario";
 
 export default function RegistroInventarioContent() {
   return (
     <div className="mx-auto max-w-[1200px]">
-        <FormularioRegistroInventario/>
+        {/* <FormularioRegistroInventario/> */}
     </div>
   );
 }
