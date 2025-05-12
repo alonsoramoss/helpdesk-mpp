1e567c0ec899956989c4bf348776420292c65d90
Alonso Ramos <alonso24005@hotmail.com>
2025-05-12 14:00:21 -0500

    style: cambia la fuente y elimina el componente Toaster de sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import { Poppins } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import type { Metadata } from "next";
 import { AuthProvider } from "@/context/authContext";
-import { Toaster } from "sonner";
 import QueryProvider from "@/providers/QueryProvider";
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
 
 export const metadata: Metadata = {
   title: "Sistema de Help Desk y Registro US",
@@ -57,9 +56,8 @@ export default function RootLayout({
     <AuthProvider>
       <QueryProvider>
         <html lang="es" suppressHydrationWarning>
-          <body className={poppins.className}>
+          <body className={montserrat.className}>
             {children}
-            <Toaster />
           </body>
         </html>
       </QueryProvider>
