42be2daa4cd46c071e774b495bf32365d99dede8
Alonso Ramos <alonso24005@hotmail.com>
2025-05-21 17:20:02 -0500

    refactor(header): mejora la obtención de la fecha actual utilizando luxon

--- src/components/admin-panel/header.tsx
+++ src/components/admin-panel/header.tsx
@@ -1,34 +1,36 @@
 import { UserHeader } from "@/components/admin-panel/user-header";
 import { MenuMovil } from "@/components/admin-panel/menu-movil";
+import { DateTime } from "luxon";
 
 interface HeaderProps {
   title: string;
 }
 
 export function Header({ title }: HeaderProps) {
-  const fechaActual = new Date().toLocaleDateString("es-ES", {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
+  const fechaActual = DateTime.now()
+    .setZone("America/Lima")
+    .setLocale("es")
+    .toLocaleString({
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
 
   return (
-    <>
-      <header className="sticky top-0 z-10 w-full bg-[#00bff9]">
-        <div className="mx-4 sm:mx-8 flex h-14 items-center">
-          <div className="flex items-center space-x-4 lg:space-x-0">
-            <MenuMovil />
-            <h1 className="font-bold">{title}</h1>
-          </div>
-          <div className="flex flex-1 items-center justify-end">
-            <UserHeader />
-          </div>
+    <header className="sticky top-0 z-10 w-full bg-[#00bff9]">
+      <div className="mx-4 sm:mx-8 flex h-14 items-center">
+        <div className="flex items-center space-x-4 lg:space-x-0">
+          <MenuMovil />
+          <h1 className="font-bold">{title}</h1>
         </div>
-        <div className="bg-neutral-200 py-1">
-          <p className="flex justify-center items-center font-medium">{fechaActual}</p>
+        <div className="flex flex-1 items-center justify-end">
+          <UserHeader />
         </div>
-      </header>
-    </>
+      </div>
+      <div className="bg-neutral-200 py-1">
+        <p className="flex justify-center items-center font-medium">{fechaActual}</p>
+      </div>
+    </header>
   );
 }
