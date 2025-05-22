38b51a5da9cc04107afb0d75709a3f0a26c1e472
Alonso Ramos <alonso24005@hotmail.com>
2025-05-22 13:46:59 -0500

    refactor(registroEquiposPerifericos): mejora las interfaces de registro de equipos con tipos de datos adecuados y agrega nuevas especificaciones técnicas

--- src/types/registroEquiposPerifericos.ts
+++ src/types/registroEquiposPerifericos.ts
@@ -1,124 +1,198 @@
 export interface RegistroEquiposPerifericosGeneral {
     // Información General
-    sede: "",
-    nomOficina: "",
+    sede: string;
+    nomOficina: string;
 
     // Equipo
-    codPatrimonialEquipo: "",
-    tipoEquipo: "",
-    fabricanteEquipo: "",
-    modeloEquipo: "",
-    numSerieEquipo: "",
-    fechAdquisicionEquipo: "",
-    estadoEquipo: "",
-    hostnameEquipo: "",
-    ipEquipo: "",
-    factorFormaEquipo: "",
-    imagenEquipo: "",
-    garantiaEquipo: "",
-    numFacturaEquipo: "",
+    codPatrimonialEquipo: string;
+    tipoEquipo: string;
+    fabricanteEquipo: string;
+    modeloEquipo: string;
+    numSerieEquipo: string;
+    fechAdquisicionEquipo: string;
+    estadoEquipo: string;
+    hostnameEquipo: string;
+    ipEquipo: string;
+    factorFormaEquipo: string;
+    imagenEquipo: string;
+    garantiaEquipo: string;
+    numFacturaEquipo: string;
 }
 
 export interface RegistroEquiposComputo extends RegistroEquiposPerifericosGeneral {
     // Placa Base
-    codPatrimonialPlaca: "",
-    fabricantePlaca: "",
-    numSeriePlaca: "",
-    modeloPlaca: "",
-    estadoPlaca: "",
-    
+    codPatrimonialPlaca: string;
+    fabricantePlaca: string;
+    numSeriePlaca: string;
+    modeloPlaca: string;
+    estadoPlaca: string;
+
     // Procesador
-    fabricanteProcesador: "",
-    estadoProcesador: "",
-    modeloProcesador: "",
-    
+    fabricanteProcesador: string;
+    estadoProcesador: string;
+    modeloProcesador: string;
+
     // Disco Duro
-    DescripcionDiscoDuro: "",
-    interfazDiscoDuro: "",
-    tipoDiscoDuro: "",
-    estadoDiscoDuro: "",
-    capacidadDiscoDuro: "",
+    DescripcionDiscoDuro: string;
+    interfazDiscoDuro: string;
+    tipoDiscoDuro: string;
+    estadoDiscoDuro: string;
+    capacidadDiscoDuro: string;
 
     // Memoria
-    moduloMemoria: "",
-    velocidadMemoria: "",
-    capacidadMemoria: "",
-    estadoMemoria: "",
+    moduloMemoria: string;
+    velocidadMemoria: string;
+    capacidadMemoria: string;
+    estadoMemoria: string;
 
     // Fuente
-    fabricanteFuente: "",
-    tipoFuente: "",
-    modeloFuente: "",
-    estadoFuente: "",
-    potenciaFuente: "",
+    fabricanteFuente: string;
+    tipoFuente: string;
+    modeloFuente: string;
+    estadoFuente: string;
+    potenciaFuente: string;
 
-    // Unidad extraible
-    unidExtraible: "",
-    estadoUnidExtraible: "",
+    // Unidad Extraible
+    unidExtraible: string;
+    estadoUnidExtraible: string;
 
-    observacion: "",
-    
+    observacion: string;
+
     // Monitor
-    codPatrimonialMonitor: "",
-    fabricanteMonitor: "",
-    numSerieMonitor: "",
-    modeloMonitor: "",
-    estadoMonitor: "",
-    tamañoMonitor: "",
-    tipoMonitor: "",
+    codPatrimonialMonitor: string;
+    fabricanteMonitor: string;
+    numSerieMonitor: string;
+    modeloMonitor: string;
+    estadoMonitor: string;
+    tamañoMonitor: string;
+    tipoMonitor: string;
 
     // Teclado
-    codPatrimonialTeclado: "",
-    fabricanteTeclado: "",
-    numSerieTeclado: "",
-    modeloTeclado: "",
-    estadoTeclado: "",
+    codPatrimonialTeclado: string;
+    fabricanteTeclado: string;
+    numSerieTeclado: string;
+    modeloTeclado: string;
+    estadoTeclado: string;
 
     // Mouse
-    codPatrimonialMouse: "",
-    fabricanteMouse: "",
-    numSerieMouse: "",
-    modeloMouse: "",
-    estadoMouse: "",
+    codPatrimonialMouse: string;
+    fabricanteMouse: string;
+    numSerieMouse: string;
+    modeloMouse: string;
+    estadoMouse: string;
 
     // Parlantes
-    codPatrimonialParlantes: "",
-    fabricanteParlantes: "",
-    numSerieParlantes: "",
-    modeloParlantes: "",
-    estadoParlantes: "",
+    codPatrimonialParlantes: string;
+    fabricanteParlantes: string;
+    numSerieParlantes: string;
+    modeloParlantes: string;
+    estadoParlantes: string;
 
     // Estabilizador
-    codPatrimonialEstabilizador: "",
-    fabricanteEstabilizador: "",
-    numSerieEstabilizador: "",
-    modeloEstabilizador: "",
-    estadoEstabilizador: "",
+    codPatrimonialEstabilizador: string;
+    fabricanteEstabilizador: string;
+    numSerieEstabilizador: string;
+    modeloEstabilizador: string;
+    estadoEstabilizador: string;
 
     // Supresor de Pico
-    codPatrimonialSupresorPico: "",
-    fabricanteSupresorPico: "",
-    numSerieSupresorPico: "",
-    modeloSupresorPico: "",
-    estadoSupresorPico: "",
+    codPatrimonialSupresorPico: string;
+    fabricanteSupresorPico: string;
+    numSerieSupresorPico: string;
+    modeloSupresorPico: string;
+    estadoSupresorPico: string;
 
-    // Tarjeta adicional
-    tarjeta: "",
-    estadoTarjeta: "",
+    // Tarjeta Adicional
+    tarjeta: string;
+    estadoTarjeta: string;
 
     // Sistema Operativo
-    tipoSO: "",
-    licenciaSistema: "Si" | "No",
+    tipoSO: string;
+    licenciaSistema: "Si" | "No";
 }
 
 export interface RegistroServidor extends RegistroEquiposPerifericosGeneral {
-    codPatrimonialServidor: string;
-    estadoServidor: string;
+    // Detalles Técnicos
+    tipoServidor: string;
+
+    // Especificaciones de Hardware
+    procesador: string;
+    frecuenciaProcesador: string;
+    numNucleos: number;
+    memoriaRam: string;
+    tipoRam: string;
+    capacidadAlmacenamiento: string;
+    tipoAlmacenamiento: string;
+    numDiscos: number;
+    interfazAlmacenamiento: string;
+    fuentePoder: string;
+    tarjetaRed: string;
+    puertosUsb: number;
+    fuenteAlimentacion: string;
+    sistemaRefrigeracion: string;
+    numVentiladores: number;
+
+    observacion: string;
+
+    // Especificaciones de Red y Conectividad 
+    interfazRed: string;
+    configuracionRed: string;
+
+    // Sistema Operativo
+    tipoSo: string;
+    versionSo: string;
+    licenciaSoftware: "Si" | "No";
+    otrosServicios: string;
+
+    // Seguridad y Respaldo
+    firewall: string;
+    proteccionSobretension: string;
+    sistemaBackup: string;
+    encriptacionDatos: string;
+    sistemaRecuperacionDesastres: string;
+    wifi: string;
 }
 
 export interface RegistroPantalla extends RegistroEquiposPerifericosGeneral {
-    codPatrimonialPantalla: string;
-    estadoPantalla: string;
+    // Detalles Técnicos
+    tipoPantalla: string;
+
+    // Especificaciones Técnicas
+    tamañoPantalla: string;
+    resolucion: string;
+    tecnologiaPantalla: string;
+    discoDuro: string;
+    memoriaRam: string;
+    conectividad: string;
+    soporteMontaje: string;
+
+    // Accesorios
+    accesorios: string;
 }
 
-export type RegistroEquiposPerifericos = RegistroEquiposComputo | RegistroServidor | RegistroPantalla;
+export interface RegistroEquipoBiometrico extends RegistroEquiposPerifericosGeneral {
+    // Especificaciones Técnicas
+    capacidadUsuarios: number;
+    capacidadHuellas: number;
+    capacidadRostros: number;
+    capacidadTarjetas: number;
+    pantalla: string;
+    comunicacion: string;
+    fuenteAlimentacion: string;
+    interfazAcceso: string;
+}
+
+
+export interface RegistroContadorBilletes extends RegistroEquiposPerifericosGeneral {
+    // Especificaciones Técnicas
+    cantidadBolsillos: number;
+    capacidadTolbo: number;
+    capacidadApisador: number;
+    capacidadRechazo: number;
+    comunicacion: string;
+    pantalla: string;
+    velocidad: string;
+    fuenteAlimentacion: string;
+}
+
+export type RegistroEquiposPerifericos = RegistroEquiposComputo | RegistroServidor | RegistroPantalla | RegistroEquipoBiometrico | RegistroContadorBilletes;
